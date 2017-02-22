726d41bf1a0fe8ae61b4c5ce6acc9ab56930cd81
Protesilaos Stavrou <info@protesilaos.com>
2017-02-22 08:49:31 +0200

    Minor changes

--- scripts/prot16_build_vim.sh
+++ scripts/prot16_build_vim.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-mkdir -p /home/prot/prot16-output/vim
-
 for filename in $(ls ~/prot16-builder/db/schemes)
 do
   name=$(echo $filename | cut -f 1 -d '.')
+  mkdir -p /home/prot/prot16-vim/
 
-  prot16-builder -s ${name} -t vim -b light > /home/prot/prot16-output/vim/${name}_light.vim
+  prot16-builder -s ${name} -t vim -b light > /home/prot/prot16-vim/${name}_light.vim
   tput setaf 2; echo "Preparing ${name} light for Vim"
-  prot16-builder -s ${name} -t vim -b dark > /home/prot/prot16-output/vim/${name}_dark.vim
+  prot16-builder -s ${name} -t vim -b dark > /home/prot/prot16-vim/${name}_dark.vim
   tput setaf 3; echo "Preparing ${name} dark for Vim"
 done
